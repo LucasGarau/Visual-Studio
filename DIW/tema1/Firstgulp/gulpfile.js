e79d45c49636badfb6f23fb374dbf9029b83d8b9
LucasGarau <48447004+LucasGarau@users.noreply.github.com>
2020-10-29 13:48:42 +0100

    m

--- DIW/tema1/Firstgulp/gulpfile.js
+++ DIW/tema1/Firstgulp/gulpfile.js
@@ -1,7 +1,8 @@
-const { series, src, dest, parallel } = require('gulp');
+const { watch,series, src, dest, parallel } = require('gulp');
 const netetjarCSS = require('gulp-clean-css');
 var uglify = require('gulp-uglify');
 var pipeline = require('readable-stream').pipeline;
+var scss = require('gulp-sass');
 
 // The `clean` function is not exported so it can be considered a private task.
 // It can still be used within the `series()` composition.
@@ -29,9 +30,22 @@ function minimitzaJS(){
   .pipe(dest('dist/js/'))
 }
 
+function compilaSCSS() {
+  return src("src/scss/*.scss")
+  .pipe(scss())
+  .pipe(dest("src/css"))
+  
+}
+
+function watcher() {
+  watch("src/scss/*.scss",compilaSCSS)
+  
+}
 
 exports.build = build;
 exports.default = series(clean, build);
 //exports.cleanifyCSS = minimitzaCSS;
 //exports.uglijss = minimitzaJS;
 exports.minify = parallel(minimitzaJS,minimitzaCSS);
+//exports.compilaSCSS = compilaSCSS;
+exports.watcher=watcher;
