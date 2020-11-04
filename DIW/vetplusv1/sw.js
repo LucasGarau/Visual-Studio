8820b5f8589616340bc519238f2f57ccce4de153
superior <lucasgarau@gmail.com>
2020-11-04 20:22:18 +0100

    m

--- DIW/vetplusv1/sw.js
+++ DIW/vetplusv1/sw.js
@@ -13,4 +13,18 @@ self.addEventListener('install', function(event) {
         return cache.addAll(urlsToCache);
       })
   );
-}); 
+});
+/*
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});*/
