aadea39aaa6c3e23e88ab2f2e40e09a106a4d2a9
LucasGarau <48447004+LucasGarau@users.noreply.github.com>
2020-11-04 13:50:22 +0100

    m

---
+++ DIW/vetplusv1/sw.js
@@ -0,0 +1,16 @@
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/DIW/vetplusv1/model/animal.js',
+  '/DIW/vetplusv1/service/AnimalService.js'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+}); 
