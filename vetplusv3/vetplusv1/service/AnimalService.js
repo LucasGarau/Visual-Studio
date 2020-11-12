3d6720bf18ae4c22bfac25808c23b41e668123d1
superior <lucasgarau@gmail.com>
2020-11-12 18:47:31 +0100

    m

--- vetplusv3/vetplusv1/service/AnimalService.js
+++ vetplusv3/vetplusv1/service/AnimalService.js
@@ -2,44 +2,50 @@ import { Animal } from '../model/Animal.js'
 
 //Retorna tots els ANIMALS (Animal.js)
 export async function getAnimals() {
-  /*
-    const animal1 = new Animal(1, 'Boby', "MASC", null, "CA");
-    const animal2 = new Animal(2, 'Llamp', "MASC", 334, "MOIX");
-    const animal3 = new Animal(3, 'Mixy', "FEM", 23423, "CA");
-    const animal4 = new Animal(4, 'Dorado', "MASC", null, "CAVALL");
-    const animal5 = new Animal(5, 'Truia', "FEM", null, "MOIX");
-    const animal6 = new Animal(6, 'Eiba', "FEM", 43434, "BOU");
-    const animal7 = new Animal(7, 'Ras', "MASC", 6455, "CA");
-   // http://server247.cfgs.esliceu.net/vetplus/getAnimals
-  */
-
-
- fetch= await("http://server247.cfgs.esliceu.net/vetplus/getAnimals")
- .then((r) => r.json())
- .then((resposta)=>{
-    return resposta;
-
- });
-
- 
- 
+    const response = await fetch('http://server247.cfgs.esliceu.net/vetplus/getAnimals');
+    const animals = await response.json();
+    return animals;
 }
 
+export async function getAnimal(idAnimal2) {
+    getAnimals().then(animals => {
+        for (let i = 0; i < animals.length; i++) { 
+            if (animals[i].idanimal=== idAnimal2) { 
+                document.querySelector("input[name='nom']").value = animals[i].nom;
+                document.querySelector("input[name='numreg']").value = animals[i].numregistre;
+                document.querySelector("input[name='sexe'][value='"+animals[i].sexe+"']").checked=true;
+                return animals[i];
+            }
+        }
+        })
+    };
+    
+    /*
+    getAnimals().then(animals => {
+        const animals2 = animals;
+        console.log(animals2);
+        console.log("getAnimal", idAnimal2);
+        return animals2.find((an) => {
+            console.log("Find", an.idAnimal, idAnimal2)
+            return an.idAnimal === idAnimal2
+        })
+});*/
 
 
-export function getAnimal(idAnimal) {
-    const animals = getAnimals();
+/*
 
-    /*for (let i = 0; i < animals.length; i++) { 
+    for (let i = 0; i < animals.length; i++) { 
         if (animals[i].id === idAnimal) { 
             return animals[i];
         }
     }
 
-    return null;*/
+   
+    /*
     console.log("getAnimal", idAnimal);
     return animals.find((an) => {
         console.log("Find", an.id, idAnimal)
         return an.id === idAnimal
     })
-}
+    */
+
