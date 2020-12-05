f0fd0e6feea0ffeccaa942d0e8eb4b8c49b05db5
LucasGarau <48447004+LucasGarau@users.noreply.github.com>
2020-12-05 20:01:00 +0100

    m

--- DIW/Kittens Gulp/gulpfile.js
+++ DIW/Kittens Gulp/gulpfile.js
@@ -3,15 +3,8 @@ const netetjarCSS = require('gulp-clean-css');
 var uglify = require('gulp-uglify');
 var pipeline = require('readable-stream').pipeline;
 var scss = require('gulp-sass');
-
-function clean(cb) {
-  cb();
-}
-
-
-function build(cb) {
-  cb();
-}
+var concat = require('gulp-concat');
+const babel = require('gulp-babel');
 
 function minimitzaCSS(){
   return src('css/*.css')
@@ -33,13 +26,34 @@ function sass() {
   
 }
 
+function concatjs(){
+  return src('dist/js/*.js')
+  .pipe(concat('all.js'))
+  .pipe(dest('dist/js/'));
+}
+
+function concatcss(){
+  return src('dist/css/*.css')
+  .pipe(concat('all.css'))
+  .pipe(dest('dist/css/'));
+}
+
+
+
+function babel(){
+  return src("dist/js/all.js")
+  .pipe(babel({
+    presets: ["@babel/preset-env"]
+  }))
+  .pipe(dest("dist/js/allbabel.js"));
+}
+
 function sasswatch() {
   watch("sass/*.scss",sass)
   
 }
 
-exports.build = build;
-exports.default = series(clean, build);
+exports.kittens = series(sass,minimitzaCSS, minimitzaJS,concatjs,concatcss,babel);
 //exports.cleanifyCSS = minimitzaCSS;
 exports.minimitzaJS = minimitzaJS;
 exports.minify = parallel(minimitzaJS,minimitzaCSS);
