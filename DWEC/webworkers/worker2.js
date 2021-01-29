4ad90919abbf082c2a727b0066e9665095a768e7
LucasGarau <48447004+LucasGarau@users.noreply.github.com>
2021-01-29 13:51:11 +0100

    m

--- DWEC/webworkers/worker2.js
+++
@@ -1,9 +0,0 @@
-
-   (async function(){
-    const fetchAeroports = await fetch("https://raw.githubusercontent.com/David-Haim/CountriesToCitiesJSON/master/countriesToCities.json")
-    const aeroports = await fetchAeroports.json();
-    postMessage(aeroports);
-   })()
-   
-
-
